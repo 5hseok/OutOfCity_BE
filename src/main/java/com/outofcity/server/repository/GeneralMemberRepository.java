250bb33e47c6f0a5b04fcd7db609fff62f1ebc89
5hseok <ohhs1010@gmail.com>
2024-11-20 19:04:32 +0900

    :sparkles: [FEAT] findByToken값으로 사용자 정보 가져오기

--- src/main/java/com/outofcity/server/repository/GeneralMemberRepository.java
+++ src/main/java/com/outofcity/server/repository/GeneralMemberRepository.java
@@ -1,8 +1,16 @@
 package com.outofcity.server.repository;
 
 import com.outofcity.server.domain.GeneralMember;
+import com.outofcity.server.global.exception.BusinessException;
+import com.outofcity.server.global.exception.message.ErrorMessage;
+import com.outofcity.server.global.jwt.JwtTokenProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GeneralMemberRepository extends JpaRepository<GeneralMember, Long> {
+
+    default GeneralMember findByToken(String token, JwtTokenProvider jwtTokenProvider) {
+        return findById(jwtTokenProvider.getUserFromJwt(token))
+                .orElseThrow(() -> new BusinessException(ErrorMessage.NOT_FOUND_USER)); // userId로 User 객체 조회
+    }
 
 }
