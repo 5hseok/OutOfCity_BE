f579087aeb6b6fd73b0676d8427e1e8414d54872
5hseok <ohhs1010@gmail.com>
2024-11-21 21:12:17 +0900

    :sparkles: [FEAT] 체험 완료한 액티비티 목록 조회 기능 추가

---
+++ src/main/java/com/outofcity/server/dto/activity/response/CompletedActivityResponseDto.java
@@ -0,0 +1,31 @@
+package com.outofcity.server.dto.activity.response;
+
+import java.util.List;
+
+public record CompletedActivityResponseDto(
+        String orderName,
+        String orderTime,
+        Integer orderPerson,
+        String orderActivityPhoto,
+        Boolean reviewExist,
+        List<Review> reviews // 리뷰를 리스트 형태로 수정
+) {
+    public record Review(
+            Long id,
+            Writer writer,
+            Integer reviewLocation,
+            Integer reviewService,
+            Integer reviewInterest,
+            Integer reviewPrice,
+            Double reviewTotalStar,
+            String reviewContent,
+            Long reviewLikeCount,
+            String createdAt
+    ) {
+        public record Writer(
+                Long id,
+                String name
+        ) {
+        }
+    }
+}
---
+++ src/main/java/com/outofcity/server/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.outofcity.server.repository;
+
+import com.outofcity.server.domain.Activity;
+import com.outofcity.server.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByActivity(Activity activity);
+}
