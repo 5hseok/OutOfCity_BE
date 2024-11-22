b01dc9bce4000a0ef99f88b3bb9b051bfd19fa97
5hseok <ohhs1010@gmail.com>
2024-11-22 15:11:17 +0900

    :sparkles: [FEAT] 사용자 닉네임 변경 기능 구현

--- src/main/java/com/outofcity/server/service/member/GeneralMemberService.java
+++ src/main/java/com/outofcity/server/service/member/GeneralMemberService.java
@@ -35,6 +35,63 @@ public class GeneralMemberService {
     private final GeneralMemberRepository generalMemberRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
+    // 사용자 정보 가져오기 - 이메일, 닉네임, 프로필 사진, 여행 등급
+    public SuccessLoginResponseDto findUserWithReact(KakaoUserInfoRequestDto userInfo) {
+
+        GeneralMember generalMember = generalMemberRepository.findById(userInfo.id())
+                .orElseGet(() -> {
+                    GeneralMember newGeneralMember = GeneralMember.builder()
+                            .id(userInfo.id())
+                            .name(userInfo.name())
+                            .email(userInfo.email())
+                            .rank("여행 씨앗")
+                            .profileImageUrl(userInfo.profileImageUrl())
+                            .build();
+                    return generalMemberRepository.save(newGeneralMember);
+                });
+
+        // JWT 토큰 발급
+        String jwtToken = jwtTokenProvider.issueAccessToken(
+                UserAuthentication.createUserAuthentication(generalMember.getGeneralMemberId())
+        );
+
+        return SuccessLoginResponseDto.builder()
+                .id(generalMember.getGeneralMemberId())
+                .name(generalMember.getName())
+                .nickname(generalMember.getNickname())
+                .rank(generalMember.getRank())
+                .profileImageUrl(generalMember.getProfileImageUrl())
+                .email(generalMember.getEmail())
+                .jwtToken(jwtToken)
+                .build();
+    }
+
+    // 일반 사용자의 여행 등급을 가져오는 메소드
+    public String getRank(String token) {
+        GeneralMember generalMember = generalMemberRepository.findById(jwtTokenProvider.getUserFromJwt(token))
+                .orElseThrow(() -> new BusinessException(ErrorMessage.GENERAL_MEMBER_NOT_FOUND));
+
+        return generalMember.getRank();
+    }
+
+    // 일반 사용자의 닉네임을 변경하는 메소드
+    public SuccessLoginResponseDto changeNickname(String token, String nickname) {
+        GeneralMember generalMember = generalMemberRepository.findById(jwtTokenProvider.getUserFromJwt(token))
+                .orElseThrow(() -> new BusinessException(ErrorMessage.GENERAL_MEMBER_NOT_FOUND));
+
+        generalMember.userUpdateGeneralMember(nickname);
+
+        return SuccessLoginResponseDto.builder()
+                .id(generalMember.getGeneralMemberId())
+                .name(generalMember.getName())
+                .nickname(generalMember.getNickname())
+                .rank(generalMember.getRank())
+                .profileImageUrl(generalMember.getProfileImageUrl())
+                .email(generalMember.getEmail())
+                .jwtToken(token)
+                .build();
+    }
+
     // Kakao Access Token 받기
     public String getAccessTokenFromKakao(String code){
         KakaoTokenResponseDto kakaoTokenResponseDto = WebClient.create(KAUTH_TOKEN_URL).post()
@@ -92,44 +149,4 @@ public class GeneralMemberService {
                 .jwtToken(jwtToken)
                 .build();
     }
-
-
-    // 사용자 정보 가져오기 - 이메일, 닉네임, 프로필 사진, 여행 등급
-    public SuccessLoginResponseDto findUserWithReact(KakaoUserInfoRequestDto userInfo) {
-
-        GeneralMember generalMember = generalMemberRepository.findById(userInfo.id())
-                .orElseGet(() -> {
-                    GeneralMember newGeneralMember = GeneralMember.builder()
-                            .id(userInfo.id())
-                            .name(userInfo.name())
-                            .email(userInfo.email())
-                            .rank("여행 씨앗")
-                            .profileImageUrl(userInfo.profileImageUrl())
-                            .build();
-                    return generalMemberRepository.save(newGeneralMember);
-                });
-
-        // JWT 토큰 발급
-        String jwtToken = jwtTokenProvider.issueAccessToken(
-                UserAuthentication.createUserAuthentication(generalMember.getGeneralMemberId())
-        );
-
-        return SuccessLoginResponseDto.builder()
-                .id(generalMember.getGeneralMemberId())
-                .name(generalMember.getName())
-                .rank(generalMember.getRank())
-                .profileImageUrl(generalMember.getProfileImageUrl())
-                .email(generalMember.getEmail())
-                .jwtToken(jwtToken)
-                .build();
-    }
-
-    // 일반 사용자의 여행 등급을 가져오는 메소드
-    public String getRank(String token) {
-        GeneralMember generalMember = generalMemberRepository.findById(jwtTokenProvider.getUserFromJwt(token))
-                .orElseThrow(() -> new BusinessException(ErrorMessage.GENERAL_MEMBER_NOT_FOUND));
-
-        return generalMember.getRank();
-    }
-
 }
