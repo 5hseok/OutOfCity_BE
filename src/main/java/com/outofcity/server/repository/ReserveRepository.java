d8c856198df9e2d110e73df9a4eb6aaf4dd17ca9
5hseok <ohhs1010@gmail.com>
2024-12-24 17:26:19 +0900

    :sparkles: [FEAT] Scheduler 서비스 사용을 위한 DAO 메소드 정의

--- src/main/java/com/outofcity/server/repository/ReserveRepository.java
+++ src/main/java/com/outofcity/server/repository/ReserveRepository.java
@@ -4,6 +4,8 @@ import com.outofcity.server.domain.Activity;
 import com.outofcity.server.domain.GeneralMember;
 import com.outofcity.server.domain.Reserve;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +13,11 @@ public interface ReserveRepository extends JpaRepository<Reserve, Long> {
     List<Reserve> findAllByGeneralMember(GeneralMember generalMember);
 
     List<Reserve> findAllByActivity(Activity activity);
+
+    @Query("SELECT r FROM Reserve r WHERE r.reserveState != 'completed'")
+    List<Reserve> findAllNotCompleted();
+
+    @Modifying
+    @Query("UPDATE Reserve r SET r.reserveState = 'completed'")
+    void updateReserveStateToCompleted(Reserve reserve);
 }
